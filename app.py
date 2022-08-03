8be8e88258a492373a02b8a94c6728ee340df996
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2022-08-03 15:34:59 +0300

    create deploy stack

--- app.py
+++ app.py
@@ -2,27 +2,15 @@
 import os
 
 import aws_cdk as cdk
+from aws_cdk import core
 
-from aws_vpc_ec2.aws_vpc_ec2_stack import AwsVpcEc2Stack
+#web app with vpc asg and alb
+from infraTemplates.vpc import VpcStack
+from infraTemplates.compute import ComputeStack
 
 
-app = cdk.App()
-AwsVpcEc2Stack(app, "AwsVpcEc2Stack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+app = core.App()
+vpc_stack = VpcStack(app, "vpc-stack")
+compute_stack = ComputeStack(app, "compute-stack", VPC=vpc_stack.VPC01)
 
 app.synth()
